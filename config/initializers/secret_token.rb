e80a34e61db8ca03aa16da933ae585c2ee97e5fc
Roland van Paridon <rolandvparidon@gmail.com>
2012-12-08 21:17:16 +0100

    Created a rails application and added status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hemloft::Application.config.secret_token = 'fff786426354587e84b2e24c5e11c9645bb0b01879c7f3d1f4e6ed02b58b1409bde5eb7872984eb0e62f35ec928f3d361ae8da82df0d130288bbfb9a9e28431f'
