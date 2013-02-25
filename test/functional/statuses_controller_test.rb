a2e588bfad46696d4a1692de6c95981e9b7ea97c
Roland van Paridon <rolandvparidon@gmail.com>
2013-02-25 09:23:24 +0100

    Scoped status updates to the current user on create and update.

--- test/functional/statuses_controller_test.rb
+++ test/functional/statuses_controller_test.rb
@@ -39,6 +39,17 @@ class StatusesControllerTest < ActionController::TestCase
     assert_redirected_to status_path(assigns(:status))
   end
 
+  test "should create status for the current user when logged in" do
+    sign_in users(:jason)
+
+    assert_difference('Status.count') do
+      post :create, status: { content: @status.content, user_id: users(:jim).id }
+    end
+
+    assert_redirected_to status_path(assigns(:status))
+    assert_equal assigns(:status).user_id, users(:jason).id
+  end
+
   test "should show status" do
     get :show, id: @status
     assert_response :success
@@ -51,14 +62,28 @@ class StatusesControllerTest < ActionController::TestCase
   end
 
   test "should redirect status when logged in" do
-    put :update, id: @status, status { content: @status.content  }  
+    put :update, id: @status, status: { content: @status.content }  
     assert_response :redirect
-    assert_redirected_tonew_user_session_path
+    assert_redirected_to new_user_session_path
   end
 
-  test "should update status when logged in" do
+  test "should update status for the current user when logged in" do
     sign_in users(:jason)
-    put :update, id: @status, status: { content: @status.content  }
+    put :update, id: @status, status: { content: @status.content, user_id: users(:jim).id }
+    assert_redirected_to status_path(assigns(:status))
+    assert_equal assigns(:status).user_id, users(:jason).id
+  end
+
+test "should not update status if nothing has changed" do
+    sign_in users(:jason)
+    put :update, id: @status
+    assert_redirected_to status_path(assigns(:status))
+    assert_equal assigns(:status).user_id, users(:jason).id
+  end
+
+ test "should update status when logged in" do
+    sign_in users(:jason)
+    put :update, id: @status, status: { content: @status.content }
     assert_redirected_to status_path(assigns(:status))
   end
 
